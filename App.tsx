029e97783e011a232a92990284ff3a1285c84310
Ilia Nikitsin <inikitsin@siriusiq.com>
2022-03-02 13:29:19 +0300

    init commit

---
+++ App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>qwertyuio</Text>
+    </View>
+  );
+};
