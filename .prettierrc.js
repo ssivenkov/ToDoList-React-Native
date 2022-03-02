924ce7a8fd2881063021d90a4c0ca4b3b3948c11
Ilia Nikitsin <inikitsin@siriusiq.com>
2022-03-02 13:29:19 +0300

    init commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  tabWidth: 2,
+  endOfLine: 'auto',
+  semi: true,
+};
