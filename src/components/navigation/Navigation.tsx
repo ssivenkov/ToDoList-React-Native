ba6b863b721cdfdca0d1642e7418fc2e899b6f8b
ssivenkov <ser.sivenkov@gmail.com>
2022-03-18 12:58:20 +0300

    add TopTabNavigator, fix app UI, fix splash screen

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -3,6 +3,7 @@ import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {View} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {ReturnComponentType} from 'types/common/returnComponentType';
 import {iconSizeMedium} from '../../constants/constants';
@@ -10,6 +11,7 @@ import {NAVIGATION} from '../../enums/navigationEnum';
 import {SignInScreen} from '../screens/signInScreen/signInScreen';
 import {styles} from './styles';
 import {TasksNavigator} from './tasksNavigation/TasksNavigator';
+import {CreateTaskListButton} from './tasksNavigation/buttons/createTaskListButton/createTaskListButton';
 import {RootTabParamListType} from './types';
 
 const RootTab = createBottomTabNavigator<RootTabParamListType>();
@@ -21,25 +23,50 @@ export const Navigation = (): ReturnComponentType => {
         <RootTab.Navigator
           initialRouteName={NAVIGATION.TASKS}
           screenOptions={({route}) => ({
-            tabBarIcon: ({focused}) => (
-              <FontAwesomeIcon
-                style={focused ? styles.tabLightIcon : styles.tabDarkIcon}
-                icon={route.name === NAVIGATION.TASKS ? faFile : faUser}
-                size={iconSizeMedium}
-              />
-            ),
-            headerShown: false,
+            headerRight: () => {
+              if (route.name === NAVIGATION.TASKS) {
+                return (
+                  <View style={styles.buttonContainer}>
+                    <CreateTaskListButton />
+                  </View>
+                );
+              }
+            },
+            headerStyle: styles.header,
+            headerTitleStyle: styles.headerTitleStyle,
             tabBarStyle: styles.tabBarContainer,
             tabBarActiveBackgroundColor: 'purple',
             tabBarActiveTintColor: '#fff',
-            tabBarInactiveTintColor: '#123',
+            tabBarInactiveTintColor: '#000',
+            tabBarIconStyle: styles.icon,
+            tabBarLabelStyle: styles.title,
           })}>
           <RootTab.Screen
             name={NAVIGATION.TASKS}
             component={TasksNavigator}
-            options={{}}
+            options={() => ({
+              tabBarIcon: ({focused}) => (
+                <FontAwesomeIcon
+                  style={focused ? styles.tabLightIcon : styles.tabDarkIcon}
+                  icon={faFile}
+                  size={iconSizeMedium}
+                />
+              ),
+            })}
           />
-          <RootTab.Screen name={NAVIGATION.SIGN_IN} component={SignInScreen} />
+          <RootTab.Screen
+            name={NAVIGATION.SIGN_IN}
+            component={SignInScreen}
+            options={() => ({
+              tabBarIcon: ({focused}) => (
+                <FontAwesomeIcon
+                  style={focused ? styles.tabLightIcon : styles.tabDarkIcon}
+                  icon={faUser}
+                  size={iconSizeMedium}
+                />
+              ),
+            })}
+          />
         </RootTab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
