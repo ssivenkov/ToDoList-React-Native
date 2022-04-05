fe81c16d55bafcfb2810ae922af466ff817f6a4f
ssivenkov <ser.sivenkov@gmail.com>
2022-04-05 12:43:39 +0300

    add sagas for Facebook actions, separating reducer actions from saga actions

--- src/store/sagas/signInSaga.ts
+++ src/store/sagas/signInSaga.ts
@@ -1,40 +1,79 @@
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {errorAlert} from '@root/helpers/Alert';
 import {
-  GetGoogleUserDataActionsType,
-  GoogleAuthCredential,
-} from '@store/actions/signInActions/types';
-import {Alert} from 'react-native';
+  GetFacebookUserDataSagaActionType,
+  GetGoogleUserDataSagaActionType,
+  AuthCredentialType,
+} from '@store/actions/signInSagaActions/types';
+import {AccessToken, LoginManager} from 'react-native-fbsdk-next';
 import {call} from 'redux-saga/effects';
 
-const signInWithCredential = (credential: GoogleAuthCredential) => {
+const commonSignInWithCredential = (credential: AuthCredentialType) => {
   return auth().signInWithCredential(credential);
 };
 
-const GoogleSignOut = () => {
+const commonSignOut = () => {
   return auth().signOut();
 };
 
-export function* googleUserDataWorker(action: GetGoogleUserDataActionsType) {
+// Google
+
+export function* googleUserDataWorker(action: GetGoogleUserDataSagaActionType) {
   const {setWaitingGoogleUserData} = action.payload;
   setWaitingGoogleUserData(true);
   try {
     const {idToken} = yield call(GoogleSignin.signIn);
-    const googleCredential: GoogleAuthCredential = yield call(
+    const googleCredential: AuthCredentialType = yield call(
       auth.GoogleAuthProvider.credential,
       idToken,
     );
-    yield call(signInWithCredential, googleCredential);
-  } catch (err: any) {
-    Alert.alert(err.toString());
+    yield call(commonSignInWithCredential, googleCredential);
+  } catch (error) {
+    if (error instanceof Error) errorAlert(error);
   }
 }
 
 export function* googleSignOutWorker() {
   try {
-    yield call(GoogleSignOut);
+    yield call(commonSignOut);
     yield call(GoogleSignin.signOut);
-  } catch (err: any) {
-    Alert.alert(err.toString());
+  } catch (error) {
+    if (error instanceof Error) errorAlert(error);
+  }
+}
+
+// Facebook
+
+export function* facebookUserDataWorker(
+  action: GetFacebookUserDataSagaActionType,
+) {
+  const {setWaitingFacebookUserData} = action.payload;
+  setWaitingFacebookUserData(true);
+  try {
+    const {isCancelled} = yield call(LoginManager.logInWithPermissions, [
+      'public_profile',
+      'email',
+    ]);
+
+    // User cancelled the login process
+    if (isCancelled) return null;
+
+    const {accessToken} = yield call(AccessToken.getCurrentAccessToken);
+    const facebookCredential: AuthCredentialType = yield call(
+      auth.FacebookAuthProvider.credential,
+      accessToken,
+    );
+    yield call(commonSignInWithCredential, facebookCredential);
+  } catch (error) {
+    if (error instanceof Error) errorAlert(error);
+  }
+}
+
+export function* facebookSignOutWorker() {
+  try {
+    yield call(commonSignOut);
+  } catch (error) {
+    if (error instanceof Error) errorAlert(error);
   }
 }
