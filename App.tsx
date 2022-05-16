c1d667c3fe64c5c2ae708f54e263bfbd52959a48
ssivenkov <ser.sivenkov@gmail.com>
2022-05-16 15:48:18 +0300

    fix notifications on iOS, add alert if iOS have no notifications permission

--- App.tsx
+++ App.tsx
@@ -1,34 +1,12 @@
 import {Navigation} from '@navigation/Navigation';
-import PushNotificationIOS from '@react-native-community/push-notification-ios';
 import {persistor, store} from '@store/store';
 import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
-import PushNotification from 'react-native-push-notification';
 import SplashScreen from 'react-native-splash-screen';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/lib/integration/react';
 
 export const App = () => {
-  useEffect(() => {
-    PushNotification.configure({
-      // (required) Called when a remote is received or opened, or local notification is opened
-      onNotification: function (notification) {
-        // (required) Called when a remote is received or opened, or local notification is opened
-        notification.finish(PushNotificationIOS.FetchResult.NoData);
-      },
-
-      // IOS ONLY (optional): default: all - Permissions to register.
-      permissions: {
-        alert: true,
-        badge: true,
-        sound: true,
-      },
-
-      popInitialNotification: true,
-      requestPermissions: true,
-    });
-  }, []);
-
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
