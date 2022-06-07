24c178fadfd1b5a9e89bef9d44257fa243fa3f50
ssivenkov <ser.sivenkov@gmail.com>
2022-06-07 11:44:18 +0300

    add React-native-linear-gradient, add gradient in sign in screen

--- android/app/src/main/java/com/myproject/MainApplication.java
+++ android/app/src/main/java/com/myproject/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
