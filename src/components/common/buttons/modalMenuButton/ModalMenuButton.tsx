c0dceeb29054525009d9f4e5510ca027d377f155
ssivenkov <ser.sivenkov@gmail.com>
2022-06-08 10:02:27 +0300

    fix UI

--- src/components/common/buttons/modalMenuButton/ModalMenuButton.tsx
+++ src/components/common/buttons/modalMenuButton/ModalMenuButton.tsx
@@ -23,7 +23,7 @@ export const ModalMenuButton = (props: ModalMenuButtonPropsType) => {
 
   const buttonGradient = theme.darkMode
     ? [COLORS.OUTER_SPACE1, COLORS.MINE_SHAFT2]
-    : [COLORS.WHITE, COLORS.DUSTY_GRAY];
+    : [COLORS.WHITE, COLORS.SILVER_CHALICE1];
 
   return (
     <LinearGradient colors={buttonGradient} style={buttonStyle}>
