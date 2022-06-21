125ba0e4708f9fe5adbe9123c0d0b29ebd0e80fc
ssivenkov <ser.sivenkov@gmail.com>
2022-06-21 18:16:11 +0300

    add flatlists

--- src/screens/tasksScreen/TaskScreen.tsx
+++ src/screens/tasksScreen/TaskScreen.tsx
@@ -5,9 +5,10 @@ import { useRoute } from '@react-navigation/native';
 import { sortingTaskLists } from '@root/helpers/sorting';
 import { useStyles } from '@root/hooks/useStyles';
 import { TaskScreenRouteType } from '@root/screens/tasksScreen/types';
+import { TaskListInterface } from '@store/reducers/tasksReducer/types';
 import { taskListsSelector } from '@store/selectors/tasksSelectors';
 import { useTranslation } from 'react-i18next';
-import { ScrollView, Text, View } from 'react-native';
+import { FlatList, ListRenderItem, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { styles } from './styles';
@@ -33,49 +34,61 @@ export const TasksScreen = () => {
   const sortedToDoTaskLists = sortingTaskLists(toDoTaskLists);
   const sortedDoneTaskLists = sortingTaskLists(doneTaskLists);
 
+  const renderToDoTaskList: ListRenderItem<TaskListInterface> = ({ item }) => {
+    const { id, date, title, tasks } = item;
+    const toDoTasks = tasks && tasks.filter((task) => !task.isDone);
+
+    return (
+      <TaskList
+        fullTaskList={item}
+        isTodoTaskList
+        key={id}
+        taskListDate={date}
+        taskListID={id}
+        taskListTasks={toDoTasks}
+        taskListTitle={title}
+      />
+    );
+  };
+
+  const renderDoneTaskList: ListRenderItem<TaskListInterface> = ({ item }) => {
+    const { id, date, title, tasks } = item;
+    const doneTasks = tasks && tasks.filter((task) => task.isDone);
+
+    return (
+      <TaskList
+        fullTaskList={item}
+        isTodoTaskList={false}
+        key={id}
+        taskListDate={date}
+        taskListID={id}
+        taskListTasks={doneTasks}
+        taskListTitle={title}
+      />
+    );
+  };
+
   if (isTodoScreen && sortedToDoTaskLists.length > 0) {
     return (
-      <ScrollView style={style.tasksListContainer}>
-        {sortedToDoTaskLists.map((toDoTaskList) => {
-          const { id, date, title, tasks } = toDoTaskList;
-          const toDoTasks = tasks && tasks.filter((task) => !task.isDone);
-
-          return (
-            <TaskList
-              fullTaskList={toDoTaskList}
-              isTodoTaskList
-              key={id}
-              taskListDate={date}
-              taskListID={id}
-              taskListTasks={toDoTasks}
-              taskListTitle={title}
-            />
-          );
-        })}
-      </ScrollView>
+      <View style={style.tasksListContainer}>
+        <FlatList
+          data={sortedToDoTaskLists}
+          keyExtractor={(toDoTaskList) => toDoTaskList.id}
+          renderItem={renderToDoTaskList}
+        />
+      </View>
     );
   }
 
   if (!isTodoScreen && sortedDoneTaskLists.length > 0) {
     return (
-      <ScrollView style={style.tasksListContainer}>
-        {sortedDoneTaskLists.map((doneTaskList) => {
-          const { id, date, title, tasks } = doneTaskList;
-          const doneTasks = tasks && tasks.filter((task) => task.isDone);
-
-          return (
-            <TaskList
-              fullTaskList={doneTaskList}
-              isTodoTaskList={false}
-              key={id}
-              taskListDate={date}
-              taskListID={id}
-              taskListTasks={doneTasks}
-              taskListTitle={title}
-            />
-          );
-        })}
-      </ScrollView>
+      <View style={style.tasksListContainer}>
+        <FlatList
+          data={sortedToDoTaskLists}
+          keyExtractor={(doneTaskList) => doneTaskList.id}
+          renderItem={renderDoneTaskList}
+        />
+      </View>
     );
   }
 
