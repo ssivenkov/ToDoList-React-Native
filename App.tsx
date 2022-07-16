98836aa906a05b92875ec192ee882f678dfa2b99
ssivenkov <ser.sivenkov@gmail.com>
2022-07-16 19:18:39 +0300

    fix husky, add pre-push husky action, fix typescript linter, fix lint script, fix lint-staged config to work correctly git-hooks, fix libraries versions, fix readme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
-import {HIDE_SPLASH_SCREEN_TIMEOUT} from '@constants/constants';
-import {Navigation} from '@navigation/Navigation';
-import {persistor, store} from '@store/store';
-import {LogBox, StatusBar} from 'react-native';
+import { HIDE_SPLASH_SCREEN_TIMEOUT } from '@constants/constants';
+import { Navigation } from '@navigation/Navigation';
+import { persistor, store } from '@store/store';
+import { LogBox, StatusBar } from 'react-native';
 import 'react-native-gesture-handler';
 import SplashScreen from 'react-native-splash-screen';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/lib/integration/react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
 export const App = () => {
   LogBox.ignoreLogs(['Sending `onAnimatedValueUpdate` with no listeners registered.']);
--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Technologies: JavaScript, TypeScript, React Native, Redux, Redux-Saga, ESLint, P
 
 Implemented: add task or task list, remove task or task list, rename task or task list, marking the completed task, mark task with any color, screens for todo tasks and for done tasks, receiving notifications at a certain date and time for tasks, sign in with Google, internet connection checking, app accent color picker button, delete account button, change app language buttons, user data storage and synchronizing with Firebase Realtime Database, dark theme mode, splash screen.
 
+**Before commit or push, please, run the script "husky install" once.**
+
 # [Download APK](https://drive.google.com/file/d/1inSXbgdNf5sVM-oS649DzsVeNQV2nv5R/view?usp=sharing "Link")
 
 ![ToDo List preview](./preview.png)
