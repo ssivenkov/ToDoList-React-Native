4b49df3b6550a8c21faa6953bf9965034ccffc38
ssivenkov <ser.sivenkov@gmail.com>
2022-09-11 19:57:13 +0300

    add default colors for color picker

--- src/components/common/colorPicker/ColorPicker.tsx
+++ src/components/common/colorPicker/ColorPicker.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { COLORS } from '@colors/colors';
 import { styles } from '@components/common/colorPicker/styles';
 import { ColorPickerComponentPropsType } from '@components/common/colorPicker/types';
 import { ColorType } from '@store/reducers/userReducer/types';
@@ -8,6 +9,16 @@ import ColorPicker from 'react-native-wheel-color-picker';
 
 export const ColorPickerComponent = (props: ColorPickerComponentPropsType) => {
   const { color, selectColor, marginTop, marginRight } = props;
+
+  const {
+    CRIMSON,
+    FLAMINGO,
+    CANDLELIGHT,
+    GREEN,
+    CERULEAN,
+    PERSIAN_BLUE,
+    ELECTRIC_VIOLET,
+  } = COLORS;
 
   const onColorChangeComplete = (color: ColorType) => {
     selectColor(color);
@@ -20,9 +31,18 @@ export const ColorPickerComponent = (props: ColorPickerComponentPropsType) => {
         discrete={false}
         noSnap={false}
         onColorChangeComplete={(color) => onColorChangeComplete(color)}
+        palette={[
+          CRIMSON,
+          FLAMINGO,
+          CANDLELIGHT,
+          GREEN,
+          CERULEAN,
+          PERSIAN_BLUE,
+          ELECTRIC_VIOLET,
+        ]}
         row
         sliderSize={40}
-        swatches={false}
+        swatches
         swatchesLast={false}
         swatchesOnly={false}
         thumbSize={40}
